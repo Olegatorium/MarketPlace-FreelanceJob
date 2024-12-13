78dfba7a4cdbaff3b432a5e1879bacc36e4d43aa
Oleh <olegatorium@gmail.com>
2024-12-13 23:41:04 +0100

    Fixed the issue for a successful deployment.

--- Program.cs
+++ Program.cs
@@ -28,10 +28,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
